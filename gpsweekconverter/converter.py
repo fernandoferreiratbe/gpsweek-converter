a9cf158becff993ba85d10e2235fac7b69afbd83
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-03-11 15:20:52 -0300

    Implement gpsweek conversion to/from julian date

--- gpsweekconverter/converter.py
+++ gpsweekconverter/converter.py
@@ -3,4 +3,17 @@
 
 class Converter:
 
-    pass
+    # noinspection PyMethodMayBeStatic
+    def convert_julian_date_to_gps_week(self, julian_date: float):
+        gps_week = ((julian_date - 2444245) // 7)
+
+        return int(gps_week)
+
+    # noinspection PyMethodMayBeStatic
+    def convert_gps_week_to_julian_date(self, gps_week: int, day_of_week: int):
+        if day_of_week not in range(0, 7):
+            raise ValueError('Day of week out of range 0-6.')
+
+        julian_day = (7.0 * gps_week + 2444245.0 + day_of_week)
+
+        return julian_day
--- gpsweekconverter/test/test_converter.py
+++ gpsweekconverter/test/test_converter.py
@@ -1,8 +1,34 @@
 # _*_ encoding: utf-8 _*_
 
 import unittest
+import datetime
+import astropy.time
+
+
+from gpsweekconverter.converter import Converter
 
 
 class TestConverter(unittest.TestCase):
 
-    pass
+    def setUp(self) -> None:
+        self.converter = Converter()
+
+    def test_given_correct_parameter_convert_julian_date_to_gps_week(self) -> None:
+        julian_date = astropy.time.Time(datetime.datetime(year=2021, month=3, day=10)).jd1
+
+        result = self.converter.convert_julian_date_to_gps_week(julian_date=julian_date)
+
+        self.assertEqual(first=2148, second=result, msg='Conversion failed. Wrong gps week calculated.')
+
+    def test_given_correct_parameter_convert_gps_week_to_julian_date(self) -> None:
+        gps_week = 2148
+        day_of_week = 3
+        result: float = self.converter.convert_gps_week_to_julian_date(gps_week=gps_week, day_of_week=day_of_week)
+
+        self.assertEqual(first=2459284.0, second=result, msg='Conversion failed. Julian date is different from expected.')
+
+    def test_given_incorrect_parameter_raise_exception_during_conversion_gps_week_to_julian_date(self):
+        gps_week = 2148
+        day_of_week = -1
+        with self.assertRaises(ValueError):
+            self.converter.convert_gps_week_to_julian_date(gps_week=gps_week, day_of_week=day_of_week)
